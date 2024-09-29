77f04d2d1694cf6e7376541b558e995b3574b912
PoliPG <hipolito.jose.perez@gmail.com>
2024-09-29 18:18:17 +0200

    candidate domain repository

--- src/sesame/Vacancy/Domain/VacancyRepository.ts
+++ src/sesame/Vacancy/Domain/VacancyRepository.ts
@@ -1,7 +1,5 @@
-import type { Candidate } from '@/sesame/Candidate/Domain/Candidate'
 import type { VacancyState } from './VacancyState'
 
 export interface VacancyRepository {
-  findVacancyCandidates(vacancyID: number): Promise<Candidate[]>
-  findVacancyStates(vacancyID: number): Promise<VacancyState>
+  findVacancyStates(vacancyID: number): Promise<VacancyState[]>
 }
