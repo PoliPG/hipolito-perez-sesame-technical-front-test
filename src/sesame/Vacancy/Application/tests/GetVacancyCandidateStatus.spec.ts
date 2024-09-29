963edaf19cb2ad319aad5cf28ebeb5bdf8da997e
PoliPG <hipolito.jose.perez@gmail.com>
2024-09-29 20:44:23 +0200

    test use case get candidate status

---
+++ src/sesame/Vacancy/Application/tests/GetVacancyCandidateStatus.spec.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata'
+import { expect, test } from 'vitest'
+import type { VacancyRepository } from '../../Domain/VacancyRepository'
+import VacancyContainerTypes from '../../VacancyContainerTypes'
+import { InMemoryVacancyRepository } from '../../Infrastructure/InMemory/InMemoryVacancyRepository'
+import { GetVacancyCandidateStatusQuery } from '../get-vacancy-candidate-status/GetVacancyCandidateStatusQuery'
+import type { Handler } from '@/sesame/Shared/EventBus/Domain/Handler'
+import { GetVacancyCandidateStatusQueryHandler } from '../get-vacancy-candidate-status/GetVacancyCandidateStatusQueryHandler'
+import { VacancyCandidateStatusDTO } from '../get-vacancy-candidate-status/VacancyCandidateStatusDTO'
+import { Container } from 'inversify'
+
+const container = new Container()
+container
+  .bind<VacancyRepository>(VacancyContainerTypes.VacancyRepository)
+  .to(InMemoryVacancyRepository)
+container
+  .bind<Handler>(VacancyContainerTypes.GetVacancyCandidateStatusQueryHandler)
+  .to(GetVacancyCandidateStatusQueryHandler)
+
+const containerTest = test.extend({
+  container: container
+})
+
+containerTest('Test use case get vacancy candidate status', async () => {
+  //Arrange
+  const query = new GetVacancyCandidateStatusQuery('c762067e-de1e-4706-9649-accad9dacb1b')
+  const queryHandler = container.get<GetVacancyCandidateStatusQueryHandler>(
+    VacancyContainerTypes.GetVacancyCandidateStatusQueryHandler
+  )
+
+  //Act
+  const candidateStatus = await queryHandler.handle(query)
+
+  //Assert
+  expect(candidateStatus[0] instanceof VacancyCandidateStatusDTO).toBe(true)
+})
