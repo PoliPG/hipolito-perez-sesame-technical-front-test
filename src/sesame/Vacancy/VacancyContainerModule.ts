3b1db05d5ce840905c698d84ed77474f4a3dfe43
PoliPG <hipolito.jose.perez@gmail.com>
2024-09-29 19:22:45 +0200

    set handler candidate in query bus

--- src/sesame/Vacancy/VacancyContainerModule.ts
+++ src/sesame/Vacancy/VacancyContainerModule.ts
@@ -5,6 +5,10 @@ import { APIVacancyRepository } from './Infrastructure/API/APIVacancyRepository'
 import VacancyContainerTypes from './VacancyContainerTypes'
 import type { Handler } from '../Shared/EventBus/Domain/Handler'
 import { GetVacancyCandidateStatusQueryHandler } from './Application/get-vacancy-candidate-status/GetVacancyCandidateStatusQueryHandler'
+import type { IocContainer } from '../Shared/IocContainer/Infrastructure/Container'
+import type { ReturningDataBus } from '../Shared/EventBus/Domain/Bus'
+import EventBusContainerTypes from '../Shared/EventBus/EventBusContainerTypes'
+import { GetVacancyCandidateStatusQuery } from './Application/get-vacancy-candidate-status/GetVacancyCandidateStatusQuery'
 
 export class VacancyContainerModule extends ContainerModule {
   constructor() {
@@ -17,5 +21,14 @@ export class VacancyContainerModule extends ContainerModule {
         .to(GetVacancyCandidateStatusQueryHandler)
         .inSingletonScope()
     })
+  }
+
+  init(container: IocContainer): void {
+    const queryBus = container.get<ReturningDataBus>(EventBusContainerTypes.QueryBus)
+
+    queryBus.register(
+      GetVacancyCandidateStatusQuery.name,
+      container.get<Handler>(VacancyContainerTypes.GetVacancyCandidateStatusQueryHandler)
+    )
   }
 }
