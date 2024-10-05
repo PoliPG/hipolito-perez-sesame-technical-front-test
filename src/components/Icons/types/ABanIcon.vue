90cc51d9fe54cb6bb3dabb51e78576ee0b6a2a3f
PoliPG <hipolito.jose.perez@gmail.com>
2024-10-05 13:54:16 +0200

    icon current color

--- src/components/Icons/types/ABanIcon.vue
+++ src/components/Icons/types/ABanIcon.vue
@@ -13,18 +13,17 @@ const { height = 20, width = 20 } = defineProps<Props>()
     :height="height"
     viewBox="0 0 24 24"
     fill="none"
+    stroke="currentColor"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
       d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"
-      stroke="#ED6F6F"
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
       d="M5.69995 5.70001L18.3 18.3"
-      stroke="#ED6F6F"
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
