476a7c7becb684d88122e47e35f1705cf027721b
PoliPG <hipolito.jose.perez@gmail.com>
2024-10-07 18:27:47 +0200

    install relative time elemet

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => ['relative-time'].includes(tag)
+        }
+      }
+    }),
+    vueDevTools()
   ],
   resolve: {
     alias: {
